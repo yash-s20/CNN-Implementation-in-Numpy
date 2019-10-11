37e48ae122ceb3b6b026e5bf70291b1b5d27c9b5
Yash Sharma <yash.sharma200999@gmail.com>
2019-10-11 18:46:29 +0530

    Task 2.1 and 2.2 complete

---
+++ autograder.py
@@ -0,0 +1,196 @@
+import sys
+import test
+import numpy as np
+from layers import *
+import pickle
+
+def grade1():
+    pathToTestCase = "testcases/testcase_01.pkl"
+    load_test_case = pickle.load(open(pathToTestCase, 'rb'))
+
+    def check_relu(task_number=1.1):
+        print('='*20 + ' TASK '+str(task_number)+ '='*20)
+        input_X = np.asarray(load_test_case['relu_input']).reshape(1,4)
+
+
+        output_X = input_X
+
+        output_X = relu_of_X(output_X)
+
+        studentAnswer = output_X
+        teacherAnswer = load_test_case['relu_output']
+
+        teacherAnswer = np.round(teacherAnswer, 6)
+        studentAnswer = np.round(studentAnswer, 6)
+
+        print('Student Answer', studentAnswer)
+        print('Correct Answer', teacherAnswer)
+        print('Correct', np.array_equal(studentAnswer, teacherAnswer))
+        return np.array_equal(studentAnswer, teacherAnswer)
+
+    def check_gradient_relu(task_number=1.2):
+        print('='*20 + ' TASK '+str(task_number)+ '='*20)
+        input_X = np.asarray(load_test_case['gardient_relu_input']).reshape(1,4)
+        input_delta = np.asarray(load_test_case['gardient_relu_input']).reshape(1,4)
+
+    
+        output_X = input_X
+
+        output_X = gradient_relu_of_X(output_X, input_delta)
+
+        studentAnswer = output_X
+        teacherAnswer = load_test_case['gardient_relu_output']
+
+        teacherAnswer = np.round(teacherAnswer, 6)
+        studentAnswer = np.round(studentAnswer, 6)
+
+        print('Student Answer', studentAnswer)
+        print('Correct Answer', teacherAnswer)
+        print('Correct', np.array_equal(studentAnswer, teacherAnswer))
+        return np.array_equal(studentAnswer, teacherAnswer)
+
+
+    def check_softmax(task_number=1.3):
+        print('='*20 + ' TASK '+str(task_number)+ '='*20)
+        input_X = np.asarray(load_test_case['softmax_input']).reshape(1,4)
+
+        output_X = input_X
+
+        output_X = softmax_of_X(output_X)
+
+        studentAnswer = output_X
+
+        teacherAnswer = load_test_case['softmax_output']
+
+        teacherAnswer = np.round(teacherAnswer, 6)
+        studentAnswer = np.round(studentAnswer, 6)
+
+        print('Student Answer', studentAnswer)
+        print('Correct Answer', teacherAnswer)
+        print('Correct', np.array_equal(studentAnswer, teacherAnswer))
+        return np.array_equal(studentAnswer, teacherAnswer)
+
+    def check_gradient_softmax(task_number=1.4):
+        print('='*20 + ' TASK '+str(task_number)+ '='*20)
+        input_X = np.asarray(load_test_case['gardient_softmax_input']).reshape(1,4)
+        input_delta = np.asarray(load_test_case['gardient_softmax_input_delta']).reshape(1,4)
+
+        output_X = input_X
+
+        output_X = gradient_softmax_of_X(output_X, input_delta)
+        
+        studentAnswer = output_X
+        teacherAnswer = load_test_case['gardient_softmax_output']
+
+        teacherAnswer = np.round(teacherAnswer, 6)
+        studentAnswer = np.round(studentAnswer, 6)
+
+        print('Student Answer', studentAnswer)
+        print('Correct Answer', teacherAnswer)
+        print('Correct', np.array_equal(studentAnswer, teacherAnswer))
+        return np.array_equal(studentAnswer, teacherAnswer)
+
+
+
+
+    np.random.seed(42)
+    print('='*20 + ' TASK 1 - Forward Pass' + '='*20)
+    marks = 0
+
+    try:
+        if check_relu():
+            marks+= 1
+    except:
+        print("Error in Test Case 1.1")
+
+    try:
+        if check_gradient_relu():
+            marks+= 1
+    except:
+        print("Error in Test Case 1.2")
+
+    try:
+        if check_softmax():
+            marks+= 1
+    except:
+        print("Error in Test Case 1.3")
+
+    try:
+        if check_gradient_softmax():
+            marks+= 2
+    except:
+        print("Error in Test Case 1.1")
+
+    
+    print('Marks: {}/5'.format(marks))
+    return marks
+
+def grade2():
+    print('='*20 + ' TASK 2 - Forward + Backward Pass' + '='*20)
+    marks = 0
+    
+    try:
+        net, xtest, ytest = test.task[1](False)
+        marks += test_net(net, xtest, ytest)
+    except:
+        print("RunTimeError in Task 2.1")
+
+
+    try:
+        net, xtest, ytest = test.task[2]()
+        marks += 3 * test_net(net, xtest, ytest)
+    except:
+        print("RunTimeError in Task 2.3")
+
+
+    try:
+        net, xtest, ytest, name = test.task[3]()
+        model = np.load(name)
+        k,i = 0,0
+        for l in net.layers:
+            if type(l).__name__ != "AvgPoolingLayer" and type(l).__name__ != "FlattenLayer": 
+                net.layers[i].weights = model[k]
+                net.layers[i].biases = model[k+1]
+                k+=2
+            i+=1
+
+        marks += 4 * test_net_diff(net, xtest, ytest)
+    except:
+        print("RunTimeError in Task 2.4")
+    
+
+    print('Marks: {}/9'.format(marks))
+    return marks
+
+
+def test_net(net, xtest, ytest):
+    _, acc  = net.validate(xtest, ytest)
+    if acc >= 90:
+        return 1
+    elif acc >= 85:
+        return 0.75
+    elif acc >= 75:
+        return 0.5
+    else:
+        return 0
+
+def test_net_diff(net, xtest, ytest):
+    _, acc  = net.validate(xtest, ytest)
+    print(acc)
+    if acc >= 35:
+        return 1
+    elif acc >= 30:
+        return 0.75
+    elif acc >= 25:
+        return 0.5
+    else:
+        return 0
+
+if len(sys.argv) < 3:
+    print('usage:\npython3 autograder.py -t task-number')
+    sys.exit(1)
+
+locals()['grade' + str(int(sys.argv[2]))]()
+
+
+
---
+++ layers.py
@@ -0,0 +1,249 @@
+import numpy as np
+
+
+class FullyConnectedLayer:
+    def __init__(self, in_nodes, out_nodes, activation):
+        # Method to initialize a Fully Connected Layer
+        # Parameters
+        # in_nodes - number of input nodes of this layer
+        # out_nodes - number of output nodes of this layer
+        self.in_nodes = in_nodes
+        self.out_nodes = out_nodes
+        self.activation = activation
+        # Stores the outgoing summation of weights * feautres
+        self.data = None
+
+        # Initializes the Weights and Biases using a Normal Distribution with Mean 0 and Standard Deviation 0.1
+        self.weights = np.random.normal(0, 0.1, (in_nodes, out_nodes))
+        self.biases = np.random.normal(0, 0.1, (1, out_nodes))
+
+    ###############################################
+    # NOTE: You must NOT change the above code but you can add extra variables if necessary
+
+    def forwardpass(self, X):
+        # print('Forward FC ',self.weights.shape)
+        # Input
+        # activations : Activations from previous layer/input
+        # Output
+        # activations : Activations after one forward pass through this layer
+
+        n = X.shape[0]  # batch size
+        # INPUT activation matrix  		:[n X self.in_nodes]
+        # OUTPUT activation matrix		:[n X self.out_nodes]
+        assert self.weights.shape[0] == X.shape[1]
+
+        ###############################################
+        # TASK 1 - YOUR CODE HERE
+        sum_l = X @ self.weights + self.biases
+        if self.activation == 'relu':
+            self.data = relu_of_X(sum_l)
+            return self.data
+        elif self.activation == 'softmax':
+            self.data = softmax_of_X(sum_l)
+            return self.data
+        else:
+            print("ERROR: Incorrect activation specified: " + self.activation)
+            exit(1)
+
+    ###############################################
+
+    def backwardpass(self, lr, activation_prev, delta):
+        # Input
+        # lr : learning rate of the neural network
+        # activation_prev : Activations from previous layer
+        # delta : del_Error/ del_activation_curr
+        # Output
+        # new_delta : del_Error/ del_activation_prev
+
+        # Update self.weights and self.biases for this layer by backpropagation
+        n = activation_prev.shape[0]  # batch size
+
+        ###############################################
+        # TASK 2 - YOUR CODE HERE
+        inp_delta = None
+        if self.activation == 'relu':
+            inp_delta = gradient_relu_of_X(self.data, delta)
+        elif self.activation == 'softmax':
+            inp_delta = gradient_softmax_of_X(self.data, delta)
+        else:
+            print("ERROR: Incorrect activation specified: " + self.activation)
+            exit()
+        del_e_w = np.mean(activation_prev[:, :, np.newaxis] @ inp_delta[:, np.newaxis, :], axis=0)
+        self.biases -= lr * np.mean(inp_delta, axis=0).reshape([1, -1])
+        self.weights -= lr * del_e_w
+        del_e_p = inp_delta @ self.weights.T
+        return del_e_p
+    ###############################################
+
+
+class ConvolutionLayer:
+    def __init__(self, in_channels, filter_size, numfilters, stride, activation):
+        # Method to initialize a Convolution Layer
+        # Parameters
+        # in_channels - list of 3 elements denoting size of input for convolution layer
+        # filter_size - list of 2 elements denoting size of kernel weights for convolution layer
+        # numfilters  - number of feature maps (denoting output depth)
+        # stride	  - stride to used during convolution forward pass
+        self.in_depth, self.in_row, self.in_col = in_channels
+        self.filter_row, self.filter_col = filter_size
+        self.stride = stride
+        self.activation = activation
+        self.out_depth = numfilters
+        self.out_row = int((self.in_row - self.filter_row) / self.stride + 1)
+        self.out_col = int((self.in_col - self.filter_col) / self.stride + 1)
+
+        # Stores the outgoing summation of weights * feautres
+        self.data = None
+
+        # Initializes the Weights and Biases using a Normal Distribution with Mean 0 and Standard Deviation 0.1
+        self.weights = np.random.normal(0, 0.1, (self.out_depth, self.in_depth, self.filter_row, self.filter_col))
+        self.biases = np.random.normal(0, 0.1, self.out_depth)
+
+    def forwardpass(self, X):
+        # print('Forward CN ',self.weights.shape)
+        # Input
+        # X : Activations from previous layer/input
+        # Output
+        # activations : Activations after one forward pass through this layer
+        n = X.shape[0]  # batch size
+        # INPUT activation matrix  		:[n X self.in_depth X self.in_row X self.in_col]
+        # OUTPUT activation matrix		:[n X self.out_depth X self.out_row X self.out_col]
+
+        ###############################################
+        # TASK 1 - YOUR CODE HERE
+        if self.activation == 'relu':
+            raise NotImplementedError
+        elif self.activation == 'softmax':
+            raise NotImplementedError
+
+        else:
+            print("ERROR: Incorrect activation specified: " + self.activation)
+            exit()
+
+    ###############################################
+
+    def backwardpass(self, lr, activation_prev, delta):
+        # Input
+        # lr : learning rate of the neural network
+        # activation_prev : Activations from previous layer
+        # delta : del_Error/ del_activation_curr
+        # Output
+        # new_delta : del_Error/ del_activation_prev
+
+        # Update self.weights and self.biases for this layer by backpropagation
+        n = activation_prev.shape[0]  # batch size
+
+        ###############################################
+        # TASK 2 - YOUR CODE HERE
+        if self.activation == 'relu':
+            raise NotImplementedError
+        elif self.activation == 'softmax':
+            raise NotImplementedError
+        else:
+            print("ERROR: Incorrect activation specified: " + self.activation)
+            exit()
+    ###############################################
+
+
+class AvgPoolingLayer:
+    def __init__(self, in_channels, filter_size, stride):
+        # Method to initialize a Convolution Layer
+        # Parameters
+        # in_channels - list of 3 elements denoting size of input for max_pooling layer
+        # filter_size - list of 2 elements denoting size of kernel weights for convolution layer
+
+        # NOTE: Here we assume filter_size = stride
+        # And we will ensure self.filter_size[0] = self.filter_size[1]
+        self.in_depth, self.in_row, self.in_col = in_channels
+        self.filter_row, self.filter_col = filter_size
+        self.stride = stride
+
+        self.out_depth = self.in_depth
+        self.out_row = int((self.in_row - self.filter_row) / self.stride + 1)
+        self.out_col = int((self.in_col - self.filter_col) / self.stride + 1)
+
+    def forwardpass(self, X):
+        # print('Forward MP ')
+        # Input
+        # X : Activations from previous layer/input
+        # Output
+        # activations : Activations after one forward pass through this layer
+
+        n = X.shape[0]  # batch size
+        # INPUT activation matrix  		:[n X self.in_depth X self.in_row X self.in_col]
+        # OUTPUT activation matrix		:[n X self.out_depth X self.out_row X self.out_col]
+
+        ###############################################
+        # TASK 1 - YOUR CODE HERE
+        raise NotImplementedError
+
+    ###############################################
+
+    def backwardpass(self, alpha, activation_prev, delta):
+        # Input
+        # lr : learning rate of the neural network
+        # activation_prev : Activations from previous layer
+        # activations_curr : Activations of current layer
+        # delta : del_Error/ del_activation_curr
+        # Output
+        # new_delta : del_Error/ del_activation_prev
+
+        n = activation_prev.shape[0]  # batch size
+
+        ###############################################
+        # TASK 2 - YOUR CODE HERE
+        raise NotImplementedError
+    ###############################################
+
+
+# Helper layer to insert between convolution and fully connected layers
+class FlattenLayer:
+    def __init__(self):
+        pass
+
+    def forwardpass(self, X):
+        self.in_batch, self.r, self.c, self.k = X.shape
+        return X.reshape(self.in_batch, self.r * self.c * self.k)
+
+    def backwardpass(self, lr, activation_prev, delta):
+        return delta.reshape(self.in_batch, self.r, self.c, self.k)
+
+
+# Function for the activation and its derivative
+def relu_of_X(X):
+    # Input
+    # data : Output from current layer/input for Activation | shape: batchSize x self.out_nodes
+    # Returns: Activations after one forward pass through this relu layer | shape: batchSize x self.out_nodes
+    # This will only be called for layers with activation relu
+    return (X > 0) * X
+
+
+def gradient_relu_of_X(X, delta):
+    # Input
+    # data : Output from next layer/input | shape: batchSize x self.out_nodes
+    # delta : del_Error/ del_activation_curr | shape: batchSize x self.out_nodes
+    # Returns: Current del_Error to pass to current layer in backward pass through relu layer | shape: batchSize x self.out_nodes
+    # This will only be called for layers with activation relu amd during backwardpass
+    return delta * (X > 0)
+
+
+def softmax_of_X(X):
+    # Input
+    # data : Output from current layer/input for Activation | shape: batchSize x self.out_nodes
+    # Returns: Activations after one forward pass through this softmax layer | shape: batchSize x self.out_nodes
+    # This will only be called for layers with activation softmax
+    exp_X = np.exp(X)
+    return exp_X / np.sum(exp_X, axis=1, keepdims=True)
+
+
+def gradient_softmax_of_X(X, delta):
+    # Input
+    # data : Output from next layer/input | shape: batchSize x self.out_nodes
+    # delta : del_Error/ del_activation_curr | shape: batchSize x self.out_nodes
+    # Returns: Current del_Error to pass to current layer in backward pass through softmax layer | shape: batchSize x self.out_nodes
+    # This will only be called for layers with activation softmax amd during backwardpass
+    # Hint: You might need to compute Jacobian first
+    identity = np.eye(X.shape[1])
+    neg_x = identity[np.newaxis, :, :] - X[:, :, np.newaxis]
+    jacob = X[:, :, np.newaxis] * np.swapaxes(neg_x, 1, 2)
+    return (delta[:, np.newaxis, :] @ np.swapaxes(jacob, 1, 2)).squeeze(axis=1)
